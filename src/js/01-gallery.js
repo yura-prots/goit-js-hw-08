8227c53303155ee985e42e2b2d1db486aa29f3f4
yura-prots <yuraprots@gmail.com>
2023-12-17 16:35:22 +0200

    task-1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkupCreator(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function galleryMarkupCreator(images) {
+  return images
+    .map(image => {
+      return `<li class="gallery__item">
+                      <a class="gallery__link" href="${image.original}">
+                          <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+                      </a>
+                </li>`;
+    })
+    .join('');
+}

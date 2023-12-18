42e032d4203a18d879dfa4baf466f592c4a42640
yura-prots <yuraprots@gmail.com>
2023-12-18 15:10:44 +0200

    task-3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+const feedback = {};
+
+populateInputs();
+
+function onInput(e) {
+  feedback[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedback));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(feedback);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateInputs() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    refs.input.value = savedMessage.email;
+    refs.textarea.value = savedMessage.message;
+  }
+}

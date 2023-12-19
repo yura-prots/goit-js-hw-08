bb7882df6754fede71f5c88b441821fbfd8fda13
yura-prots <yuraprots@gmail.com>
2023-12-19 14:26:16 +0200

    task-2 corrected

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on(
   'timeupdate',
   throttle(e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+    localStorage.setItem(CURRENT_TIME, e.seconds);
   }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
